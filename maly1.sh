dcc1dd2559a08d02985cb1a4c2882a1499b19750
lukaszgolojuch <lukasz.golojuch@gmail.com>
2021-05-15 12:11:35 +0200

    code

---
+++ maly1.sh
@@ -0,0 +1,122 @@
+﻿#!/bin/bash
+
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+RESET=`tput sgr0`
+
+onPingOperations(){
+
+echo "Please give me url: "
+read strona
+
+echo "Connecting..."
+ping -c1 $strona > /dev/null 2>&1
+
+  if [ $? -eq 0 ]
+  then 
+	echo "${GREEN}Succesful connection ${RESET}"
+	echo_ping=$(ping -c1 $strona | sed -nE 's/^PING[^(]+\(([^)]+)\).*/\1/p')
+	echo "Ip adress for $strona: $echo_ping"
+  else
+	echo "${RED}Ip cannot be found... try again later ${RESET}"
+  fi
+}
+
+XServer(){
+echo "Here is the most important info about your Xserver:"
+echo ""
+echo $(grep 'Build Operating System' /var/log/Xorg.0.log)
+echo $(grep 'Current Operating System' /var/log/Xorg.0.log)
+echo $(grep 'Kernel command line' /var/log/Xorg.0.log)
+echo $(grep 'Current Operating System' /var/log/Xorg.0.log)
+echo $(grep 'Build Date' /var/log/Xorg.0.log)
+}
+
+eth0Info(){
+echo "Here is the most important info about your eth0:"
+echo ""
+echo_ifconfig=$(ifconfig | grep 'broadcast')
+echo $echo_ifconfig
+}
+
+processorInfo(){
+echo "Here is the most important info about your processor:"
+echo ""
+lscpu | grep 'Model name' | head -n1
+lscpu | grep 'CPU op-mode' | head -n1
+lscpu | grep 'Architecture' | head -n1
+lscpu | grep 'CPU MHz' | head -n1
+lscpu | grep 'Hypervisor vendor' | head -n1
+lscpu | grep 'L1d cache' | head -n1
+lscpu | grep 'L1i cache' | head -n1
+lscpu | grep 'L2 cache' | head -n1
+lscpu | grep 'L3 cache' | head -n1
+}
+
+systemInfo(){
+echo "Here is the most important info about your operating system:"
+echo ""
+sysinfo=$(sudo lshw -short | grep 'system')
+echo $sysinfo
+}
+
+motherboardInfo(){
+echo "Here is the most important info about your motherboard:"
+echo ""
+mtboardinfo=$(sudo lshw -short | grep 'bus')
+echo $mtboardinfo
+}
+
+compInfo(){
+echo "Here is the most important info about your computer:"
+echo ""
+sudo dmidecode -t system | grep 'Manufacturer' 
+sudo dmidecode -t system | grep 'Product Name' 
+sudo dmidecode -t system | grep 'Version' 
+sudo dmidecode -t system | grep 'Serial Number' 
+}
+
+echo "=================================================="
+echo "Author: Lukasz Golojuch"
+echo "Operating system: Linux"
+echo "Version of system: Knoppix 9.1"
+echo "Language: bash"
+echo "=================================================="
+echo "Program description: grep functionality usage"
+echo "=================================================="
+echo "Hello $USER"
+echo ""
+echo "Menu"
+echo "1. Return IP adress from URL adress"
+echo "2. Info about X server"
+echo "3. Info about eth0"
+echo "4. Info about processor"
+echo "5. Info about system"
+echo "6. Info about motherboard"
+echo "7. Info about computer (serial number, manufacturer etc.)"
+echo ""
+echo "Twoj wybor:"
+read answ
+
+ if [ $answ -eq 1 ]
+ then 
+	onPingOperations
+ elif [ $answ -eq 2 ]
+ then
+	XServer
+ elif [ $answ -eq 3 ]
+ then
+	eth0Info
+ elif [ $answ -eq 4 ]
+ then
+	processorInfo
+ elif [ $answ -eq 5 ]
+ then
+	systemInfo
+ elif [ $answ -eq 6 ]
+ then
+	motherboardInfo
+ elif [ $answ -eq 7 ]
+ then
+	compInfo
+ fi
---
+++ readme.md
@@ -0,0 +1,29 @@
+# About
+
+    Program name: „Grep usage”
+    Programming language: Bash
+    Author: Łukasz Gołojuch
+    Operating system: Linux Knoppix
+    
+## Program description
+
+The program mainly uses the grep and sed commands to extract the most important information from the logs, or to respond to system commands.
+
+## Functions 
+
+onPingOperations() - function that gives us information about IP of url or info about wrong url 
+
+XServer() - function that gives us the most important information about XServer log. If program does not work please check direction of your XServer log file.
+
+eth0Info() - function that gives us the most important info about your eth0:
+
+processorInfo() - function that gives us the most important info about our processor
+
+systemInfo() - function that gives us the most important info about your operating system
+
+motherboardInfo() -  function that gives us the most important info about your motherboard
+
+compInfo() -  function that gives us the most important info about your computer device
+
+## Licencja
+[Open Source]
